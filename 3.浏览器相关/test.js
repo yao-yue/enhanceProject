d7e9e73ee4d203882e683fff411581673d76dddf
hbw <2316570512@qq.com>
2020-03-05 22:01:31 +0800

    跨域

---
+++ 3.浏览器相关/test.js
@@ -0,0 +1,10 @@
+// server.js
+let express = require('express')
+let app = express()
+app.get('/say', function(req, res) {
+  let { wd, callback } = req.query
+  console.log(wd) // Iloveyou
+  console.log(callback) // show
+  res.end(`${callback}('我不爱你')`)
+})
+app.listen(3000)
