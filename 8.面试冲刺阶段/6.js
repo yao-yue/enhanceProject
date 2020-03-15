7e95c77b0b2b304a4877d0768a1ab3588cb2781f
hbw <2316570512@qq.com>
2020-03-16 00:05:46 +0800

    so tird.  back

--- 8.面试冲刺阶段/6.js
+++ 8.面试冲刺阶段/6.js
@@ -1,9 +1,13 @@
 // 心乱不如给自己计划，要把要解决掉的给规划出来
-1. js手写系列
-手写call\apply\bind\new
-手写节流、防抖、强化节流
-封装ajax 手写promise 封装jsonp
+// 1. js手写系列
+// 手写call\apply\bind\new
+// 手写节流、防抖、强化节流
+// 封装ajax 手写promise 封装jsonp
+// 组合继承
+// 深拷贝
+// 去重、扁平数组、 
 
-2. 算法系列
-3. react系列问题
-4. 计算机基础知识
+// 2. 算法系列
+// 3. react系列问题
+// setState hooks 
+// 4. 计算机基础知识
